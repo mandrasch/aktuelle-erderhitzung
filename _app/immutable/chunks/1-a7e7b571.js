e7ecbb235272654a47a3fae1f28c9d9b023365f5
mandrasch <777278+mandrasch@users.noreply.github.com>
2023-01-22 13:59:35 +0000

    Deploying to gh-pages from @ mandrasch/wie-steht-es-um-das-klima-so@b0136098e5162ad600fff3856ac4b631adb42260 🚀

--- _app/immutable/chunks/1-a7e7b571.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-e358026c.js";export{t as component};
---
+++ _app/immutable/chunks/2-0a5c3719.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-d422e891.js";export{t as component};
