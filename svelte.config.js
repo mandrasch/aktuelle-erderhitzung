8bf3e4a33082b6456c1c7ecd9799f4025aac710b
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2023-01-22 14:38:16 +0100

    ci: add adapter-static for gh pages (WIP)

--- svelte.config.js
+++ svelte.config.js
@@ -6,6 +6,20 @@ const config = {
 	preprocess: sveltePreprocess(),
 	kit: {
 		adapter: adapter()
+	},
+	// https://stackoverflow.com/a/72733222
+	adapter: adapter({
+		pages: 'build',
+		assets: 'build',
+		fallback: null,
+		precompress: false
+	}),
+	prerender: {
+		default: true,
+	},
+	trailingSlash: 'always',
+	paths: {
+		base: '/wie-steht-es-um-das-klima-so'
 	}
 };
 
