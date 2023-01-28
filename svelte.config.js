301a18e17ccf3dd2881b2974c8bebd708678e074
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2023-01-28 21:13:03 +0100

    ci: set paths.base via deploy script (WIP/experimental)

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,16 @@
 import adapter from '@sveltejs/adapter-static';
 import sveltePreprocess from 'svelte-preprocess';
 
+import { env } from '$env/dynamic/private';
+
+let pathsBase = '';
+if(Object.prototype.hasOwnProperty.call(env,'pathsBase')){
+	console.log('paths.base was set via node env variable',env.PATHS_BASE);
+	pathsBase = env.PATHS_BASE;
+}else{
+	console.log('No paths.base set, defaults to \'\'');
+}
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: sveltePreprocess(),
@@ -19,7 +29,9 @@ const config = {
 		// This was removed? 
 		// trailingSlash: 'always',
 		paths: {
-			base: '/wie-steht-es-um-das-klima-so'
+			base: pathsBase
+			// e.g. for github pagees: '/wie-steht-es-um-das-klima-so'
+			//https://kit.svelte.dev/docs/adapter-static#github-pages
 		}
 	}
 };
