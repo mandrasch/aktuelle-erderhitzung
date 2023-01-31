8b5dafaa84ee1b1c37ae969c38929e3e8c0e8675
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2023-01-31 21:07:39 +0100

    ci: set paths.base via deploy script (WIP/experimental)

--- svelte.config.js
+++ svelte.config.js
@@ -2,14 +2,16 @@ import adapter from '@sveltejs/adapter-static';
 import sveltePreprocess from 'svelte-preprocess';
 
 // https://kit.svelte.dev/docs/modules#$env-static-public
-import { PATHS_BASE } from '$env/static/public';
+// import { PATHS_BASE } from '$env/static/public';
+
+console.log(process.env.PATHS_BASE);
 
 // TODO: use env from github actions -> 
 let pathsBase = '';
 // https://stackoverflow.com/posts/67978668/revisions
-if (PATHS_BASE != undefined) {
+if (process.env.PATHS_BASE != undefined) {
 	console.log('paths.base was set via node env variable', process.env.PATHS_BASE);
-	pathsBase = PATHS_BASE;
+	pathsBase = process.env.PATHS_BASE;
 } else {
 	console.log('No paths.base set, defaults to \'\'');
 }
